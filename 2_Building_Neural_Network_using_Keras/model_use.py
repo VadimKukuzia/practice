98178728cbf12f5895d220db9409b5e8fe1fbb67
Vadim Kukuzia <vadimol081@gmail.com>
2021-02-12 15:30:01 +0200

    Made simple model with Keras

---
+++ 2_Building_Neural_Network_using_Keras/model_use.py
@@ -0,0 +1,35 @@
+import numpy as np
+import mnist
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+
+# Build the model.
+model = Sequential([
+    Dense(64, activation='relu', input_shape=(784,)),
+    Dense(64, activation='relu'),
+    Dense(10, activation='softmax'),
+])
+
+# Load the model's saved weights.
+model.load_weights('model.h5')
+
+train_images = mnist.train_images()
+train_labels = mnist.train_labels()
+test_images = mnist.test_images()
+test_labels = mnist.test_labels()
+
+# Normalize the images.
+train_images = (train_images / 255) - 0.5
+test_images = (test_images / 255) - 0.5
+
+# Flatten the images.
+train_images = train_images.reshape((-1, 784))
+test_images = test_images.reshape((-1, 784))
+
+predictions = model.predict(test_images[:10])
+
+# Print our model's predictions.
+print(np.argmax(predictions, axis=1))  # [7, 2, 1, 0, 4]
+
+# Check our predictions against the ground truths.
+print(test_labels[:10])  # [7, 2, 1, 0, 4]
