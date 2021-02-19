016feffd9a6af7b5d9de81e613495a769afc9cc4
Vadim Kukuzia <vadimol081@gmail.com>
2021-02-19 17:27:25 +0200

    Models

--- 2_Building_Neural_Network_using_Keras/model_train.py
+++ 2_Building_Neural_Network_using_Keras/model_train.py
@@ -1,11 +1,15 @@
+from datetime import datetime
+
 import numpy as np
 import mnist
-from matplotlib import ticker
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Dense, Dropout
 from tensorflow.keras.utils import to_categorical
 import matplotlib.pyplot as plt
 from contextlib import redirect_stdout
+import os
+
+from tensorflow.keras.optimizers import Adamax
 
 train_images = mnist.train_images()
 train_labels = mnist.train_labels()
@@ -31,10 +35,9 @@ print(model.summary())
 # Save model summary
 
 
-
 # Compile the model.
 model.compile(
-    optimizer='adam',
+    optimizer=Adamax(learning_rate=0.001),
     loss='categorical_crossentropy',
     metrics=['accuracy'],
 )
@@ -44,14 +47,15 @@ model.compile(
 history = model.fit(
     train_images,
     to_categorical(train_labels),
-    epochs=5,
+    epochs=10,
     batch_size=32,
     validation_split=0.1,
+    validation_data=(test_images, to_categorical(test_labels))
 )
 
-
 print('Train accuracy: Min - ', min(history.history['accuracy']), ' |', 'Max - ', max(history.history['accuracy']))
-print('Validation accuracy: Min - ', min(history.history['val_accuracy']), ' |', 'Max - ', max(history.history['val_accuracy']))
+print('Validation accuracy: Min - ', min(history.history['val_accuracy']), ' |', 'Max - ',
+      max(history.history['val_accuracy']))
 print('Train loss: Max - ', max(history.history['loss']), ' |', 'Min - ', min(history.history['loss']))
 print('Validation loss: Max -', max(history.history['val_loss']), ' |', 'Min - ', min(history.history['val_loss']))
 # print(history.history.keys())
@@ -60,6 +64,9 @@ print('Validation loss: Max -', max(history.history['val_loss']), ' |', 'Min - '
 # print(history.history['loss'])
 # print(history.history['val_loss'])
 
+# time for result saving. creating new model directory
+time = datetime.strftime(datetime.now(), "%d.%m.%Y_%H.%M.%S")
+os.mkdir(f'models_n_results/model_{time}')
 
 fig, (ax1, ax2) = plt.subplots(1, 2)
 fig.suptitle('Model accuracy and loss')
@@ -80,7 +87,7 @@ ax2.grid()
 ax1.plot(history.history['accuracy'])
 ax1.plot(history.history['val_accuracy'])
 
-ax1.set_title(f'Model accuracy. Train min = ')
+ax1.set_title(f'Model accuracy')
 ax1.set_ylabel('Accuracy')
 ax1.set_xlabel('Epoch')
 ax1.legend(['Train', 'Validation'], loc='best')
@@ -93,6 +100,7 @@ ax2.set_ylabel('Loss')
 ax2.set_xlabel('Epoch')
 ax2.legend(['Train', 'Validation'], loc='best')
 
+plt.savefig(f'models_n_results/model_{time}/Accuracy_loss.png')
 plt.show()
 #  "Accuracy"
 # plt.plot(history.history['accuracy'])
@@ -117,13 +125,12 @@ model.evaluate(
     to_categorical(test_labels)
 )
 
-with open('models_n_results/model.txt', 'w') as f:
+with open(f'models_n_results/model_{time}/model.txt', 'w') as f:
     with redirect_stdout(f):
         model.summary()
 
 # Save the model to disk.
-model.save_weights('model.h5')
-
+model.save_weights(f'models_n_results/model_{time}/model.h5')
 # Load the model from disk later using:
 # model.load_weights('model.h5')
 
